e5d03e44f88b22b1f7fefd26746a8e9cf03a080a
Franz <francesco.romeo-work@libero.it>
2022-12-01 10:58:55 +0100

    Rendering Addition

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:exam_project/entities/tv_show_entity.dart';
 import 'package:exam_project/views/tv_show/tv_show_bloc.dart';
 import 'package:flutter/material.dart';
 
@@ -16,8 +17,6 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     _tvShowBloc.getTvShows();
-    print('Hello');
-    print(_tvShowBloc.showsList);
     super.initState();
   }
 
@@ -37,8 +36,29 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(),
-        body: Center(
-          child: CircularProgressIndicator(),
-        ));
+        body: FutureBuilder<List<TvShowEntity>>(
+          future: _tvShowBloc.getTvShows(),
+          builder: (BuildContext context,
+              AsyncSnapshot<List<TvShowEntity>> snapshot) {
+            if (snapshot.data == null) {
+              return const CircularProgressIndicator();
+            }
+            final List<TvShowEntity> data = snapshot.data!;
+            return ListView.builder(
+                padding: const EdgeInsets.all(8),
+                itemCount: snapshot.data!.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                      child: ListTile(
+                      leading: Image.network(data[index].image!),
+                  title: Text(data[index].title!),
+                  subtitle: Text("Rating: ${data[index].rating!}"),
+                  trailing: const Icon(Icons.more_vert),
+                  ));
+                  }
+            );
+          },
+        )
+    );
   }
 }
