babb2c126dea0e5c2324f46250d12fed5ff8a92e
Franz <francesco.romeo-work@libero.it>
2022-12-03 20:18:59 +0100

    File Renamed

--- lib/api/tv_show_remote.dart
+++ lib/api/tv_show_remote.dart
@@ -3,22 +3,26 @@ class TvShowRemote {
   String title = '';
   String description = '';
   String image = '';
+  List<dynamic> genres = <dynamic>[];
   num? rating;
+  String status = '';
 
   TvShowRemote(
       {required this.id,
       required this.title,
       required this.description,
       required this.image,
-      required this.rating});
+      required this.genres,
+      required this.rating,
+      required this.status});
 
   TvShowRemote.fromJson(Map<String, dynamic> json) {
     id = json['id'].toString();
     title = json['name'];
     description = json['summary'];
+    genres = json['genres'];
     image = json['image']['original'];
     rating = json['rating']['average'];
+    status = json['status'];
   }
 }
-
-
